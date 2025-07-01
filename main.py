b56f4b4039967df5df7f1308204414a6e029dc7c
William Blackerby <wmblackerby@gmail.com>
2025-07-01 15:41:47 -0400

    add df for all meetings

--- main.py
+++ main.py
@@ -15,6 +15,19 @@ meetings = data["meetings"]
 
 df = pl.DataFrame(meetings)
 
+st.subheader("House Events Next Seven Days")
+
+st.dataframe(
+    df,
+    use_container_width=True,
+    column_config={
+        "cdg_api_url_json": st.column_config.LinkColumn(),
+        "cdg_api_url_xml": st.column_config.LinkColumn(),
+        "cdg_url": st.column_config.LinkColumn(),
+        "house_repo_url": st.column_config.LinkColumn(),
+    },
+)
+
 if len(df) > 0:
     df = df.filter(
         ((pl.col("cdg_json_status") != 200) | (pl.col("cdg_xml_status") != 200))
