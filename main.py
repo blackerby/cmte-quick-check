b23a4f857bc263c4388b8592190ff249f7505a04
William Blackerby <wmblackerby@gmail.com>
2025-07-30 09:40:57 -0400

    conditional error table creation

--- main.py
+++ main.py
@@ -34,28 +34,28 @@ if len(df) > 0:
     )
     stat_df = df.filter(pl.col("cdg_status") != pl.col("repo_status"))
 
-st.subheader("House Events with Error Codes in CDG")
+    st.subheader("House Events with Error Codes in CDG")
 
-st.dataframe(
-    err_df,
-    use_container_width=True,
-    column_config={
-        "cdg_api_url_json": st.column_config.LinkColumn(),
-        "cdg_api_url_xml": st.column_config.LinkColumn(),
-        "cdg_url": st.column_config.LinkColumn(),
-        "house_repo_url": st.column_config.LinkColumn(),
-    },
-)
+    st.dataframe(
+        err_df,
+        use_container_width=True,
+        column_config={
+            "cdg_api_url_json": st.column_config.LinkColumn(),
+            "cdg_api_url_xml": st.column_config.LinkColumn(),
+            "cdg_url": st.column_config.LinkColumn(),
+            "house_repo_url": st.column_config.LinkColumn(),
+        },
+    )
 
-st.subheader("House Events with Mismatched Statuses")
+    st.subheader("House Events with Mismatched Statuses")
 
-st.dataframe(
-    stat_df,
-    use_container_width=True,
-    column_config={
-        "cdg_api_url_json": st.column_config.LinkColumn(),
-        "cdg_api_url_xml": st.column_config.LinkColumn(),
-        "cdg_url": st.column_config.LinkColumn(),
-        "house_repo_url": st.column_config.LinkColumn(),
-    },
-)
+    st.dataframe(
+        stat_df,
+        use_container_width=True,
+        column_config={
+            "cdg_api_url_json": st.column_config.LinkColumn(),
+            "cdg_api_url_xml": st.column_config.LinkColumn(),
+            "cdg_url": st.column_config.LinkColumn(),
+            "house_repo_url": st.column_config.LinkColumn(),
+        },
+    )
